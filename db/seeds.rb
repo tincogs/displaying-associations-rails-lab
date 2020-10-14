1fe7225ca41986b9ae2c7e53a36b31bff0a80756
tincogs <tomoday91@gmail.com>
2020-10-14 11:42:05 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+clickbait = Artist.create!(name: "Motivation")
+clickbait.songs.create!(title: "10 Ways You Are Already Awesome")
+clickbait.songs.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
+clickbait.songs.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
+
+movies = Artist.create!(name: "Movies")
+movies.songs.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")
